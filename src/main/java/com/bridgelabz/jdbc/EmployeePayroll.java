641adb902c19297d9d8d501f10b5db82c733646e
rajivjajo <rajivjajodia82552@gmail.com>
2023-02-27 02:03:45 +0530

    Added: Adding terissa into employee payroll table

--- src/main/java/com/bridgelabz/jdbc/EmployeePayroll.java
+++ src/main/java/com/bridgelabz/jdbc/EmployeePayroll.java
@@ -20,8 +20,9 @@ public class EmployeePayroll {
             con = DriverManager.getConnection(jdbcURL, username, password);
             System.out.println("connection done successful!!" + con);
             Statement statement=con.createStatement();
-            statement.execute("alter table employee add column phone varchar(30)");
-            statement.execute("alter table employee add column address varchar(90)");
+            // statement.execute("alter table employee add column phone varchar(30)");
+            statement.executeUpdate("update employee set name='Terissa',\n" +
+                    "department='sales-marketing' where id=2;");
             ResultSet resultSet =statement.executeQuery("select * from employee");
             while(resultSet.next()){
                 System.out.println("id:"+resultSet.getInt("id"));
